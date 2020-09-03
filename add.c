31308f01472bfaac3b38d878ea900a0d3b07537f
Ashish S <ashishjsharda@gmail.com>
2020-09-03 07:58:15 -0400

    Create add.c

---
+++ add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Basic C addition
+ * @return  int
+ * @author ashish
+ */
+int main() {
+    int a,b,c;
+    a=10;
+    b=20;
+    c=a+b;
+    printf("%d",c);
+    return 0;
+}

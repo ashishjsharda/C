44a919e5422f110c6d675c75923bb1634e132388
Ashish S <ashishjsharda@gmail.com>
2020-09-04 08:13:42 -0400

    Create constants.c

---
+++ constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Using Constants
+ * @return  int
+ * @author ashish
+ */
+#define SIDE 10
+
+int main() {
+    int area;
+    area=SIDE*SIDE;
+    printf("%d",area);
+    return 0;
+}

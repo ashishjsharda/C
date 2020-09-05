b8c100c5f9de000c06462ec69acb06b47445d944
Ashish S <ashishjsharda@gmail.com>
2020-09-05 10:02:24 -0400

    Create typecast.c

---
+++ typecast.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Example using Typecasting
+ */
+int main() {
+    int sum=18,count =5;
+    double mean ;
+    mean =(double)sum/count;
+    printf(" Mean is %f" ,mean);
+    return 0;
+}
